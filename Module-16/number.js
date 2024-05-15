9b9b9fbde7087856f4e98739b4f454a15a1808a6
MD IMRUL MOLLA <imrulmolla99@gmail.com>
2024-05-15 22:58:03 +0600

    added the module-16 item

--- Module-16/number.js
+++ Module-16/number.js
@@ -9,3 +9,24 @@ var first = 0.1;
 var second = 0.2;
 var totall = first + second;
 console.log(typeof(totall.toFixed(3)))
+
+
+//**********  Module 16-7  *********/
+
+var helself = 300;
+var myself = 155;
+
+console.log(helself + myself); // add
+console.log(helself - myself); //
+console.log(helself * myself);
+console.log(helself / myself);
+
+var EnglishMarks = 75;
+var BanglaMarks = 99;
+var TotalMark = EnglishMarks / BanglaMarks;
+console.log(TotalMark.toFixed(5))
+
+
+// Remainder
+
+console.log(EnglishMarks % BanglaMarks);
