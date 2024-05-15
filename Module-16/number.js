a77c381ebf22fb70a8e4379225c1edc050a74ce2
MD IMRUL MOLLA <imrulmolla99@gmail.com>
2024-05-15 22:42:23 +0600

    first commit

---
+++ Module-16/number.js
@@ -0,0 +1,11 @@
+var orangePrice = 20;
+var mangoPrice = '55.55';
+var mangoPrice = parseFloat('55.55')
+var total = orangePrice + mangoPrice;
+console.log(total)
+
+
+var first = 0.1;
+var second = 0.2;
+var totall = first + second;
+console.log(typeof(totall.toFixed(3)))
