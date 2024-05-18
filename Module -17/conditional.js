ad5e23eca9b2b3d0e3d1eebc6a0f0028443a721d
imrul099 <imrulmolla99@gmail.com>
2024-05-18 13:12:40 +0600

    solved a prblem of conditional

--- Module -17/conditional.js
+++ Module -17/conditional.js
@@ -73,3 +73,23 @@ else if(price >= 3000){
 else{
     console.log('nothing to say')
 }
+
+
+
+
+// const age = 50;
+// const age = 18;
+const age = 61;
+const bergePrice = 500;
+
+if(age <= 18){
+    console.log('you can eat for free')
+}
+else if(age >= 60){
+    const discount = bergePrice * 10/100;
+    const payAmount = bergePrice - discount;
+    console.log(payAmount);
+}
+else{
+    console.log(bergePrice)
+}
