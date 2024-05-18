94ad437dde15b85d1051daf7f04cce92f48ec929
imrul099 <imrulmolla99@gmail.com>
2024-05-18 15:27:55 +0600

    added the nested condition

---
+++ Module -17/nested-condition.js
@@ -0,0 +1,16 @@
+// const money = 500;
+// const money = 200;
+const money = 100;
+if(money > 300){
+    console.log('yoou are rich')
+}
+else{
+    if(money > 100){
+        console.log('tui gorib o na borolok o na')
+    }
+    else{
+        if (money > 0){
+            console.log('kmaon acho tmi')
+        }
+    }
+}
