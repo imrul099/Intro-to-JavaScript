42ad5e970801bfbac8ffc4cd1f13132225662248
imrul099 <imrulmolla99@gmail.com>
2024-06-05 09:52:22 +0600

    added the function details

--- Module-20/function.js
+++ Module-20/function.js
@@ -6,3 +6,27 @@
 
  //call the function
  brushTeeth();
+
+
+ function square(number){
+    console.log('value of the number', number);
+    const borgo = number * number;
+    console.log(borgo)
+ }
+
+ square(4);
+
+ function add(num1, num2){
+    const addition = num1 + num2;
+    console.log(addition);
+ }
+
+ add(4, 6);
+
+
+ function addAll(a, b, c, d){
+    const total = a + b + c + d;
+    console.log(a, b, c, d);
+    console.log(total);
+ }
+ addAll(2,4,6);
