31cacf06c17c9a799fee727ec374aad0671896f3
imrul099 <imrulmolla99@gmail.com>
2024-06-10 12:19:27 +0600

    added multi layer discout using function

---
+++ Module-22/layer-discount.js
@@ -0,0 +1,36 @@
+/**
+ * first100 - 100;
+ * 101T0200 - 90;
+ * avobe - 70;
+ */
+
+function layerDiscount(quantity){
+    const first100Price = 100;
+    const second100Price = 90;
+    const avobe200Price = 70;
+
+    if(quantity <= 100){
+        const total = quantity * first100Price;
+        console.log(total);
+    }
+    else if(quantity <= 200){
+        const first100Total = 100 * first100Price;
+
+        const remainingQuantity = quantity - 100;
+        const remainingTotal = remainingQuantity * second100Price;
+        const total = first100Total + remainingTotal;
+        console.log(total);
+    }
+    else{
+        const first100Total = 100* first100Price;
+        const second100Total = 100 * second100Price;
+
+        const remainingQuantity = quantity - 200;
+        const remainingTotal = remainingQuantity * avobe200Price;
+        const total = first100Total + second100Total + remainingTotal;
+        console.log(total);
+
+    }
+}
+
+layerDiscount(350);
