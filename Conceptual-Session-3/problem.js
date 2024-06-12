d26080073eaf2fd62a36ebab264d10039f8f6ca7
imrul099 <imrulmolla99@gmail.com>
2024-06-12 08:32:17 +0600

    added a solution using function

--- Conceptual-Session-3/problem.js
+++ Conceptual-Session-3/problem.js
@@ -10,4 +10,29 @@ function AnaToVori(Ana){
 }
 
 const vori = AnaToVori('5');
-console.log(vori);
+//console.log(vori);
+
+
+//Problem -2;
+
+function pandaCost(singara, samusa, jilapi){
+    if(typeof singara !== 'number' || typeof samusa !== 'number' || typeof jilapi !== 'number'){
+        return 'plese use valid number';
+    }
+    else if(singara < 0 || samusa < 0 || jilapi < 0){
+        return 'use positive number';
+    }
+    else{
+        const singaraPrice = 7;
+    const samusaPrice = 10;
+    const jilapiPrice = 15;
+
+    const total = singara * singaraPrice + samusa * samusaPrice + jilapi * jilapiPrice;
+    return total;
+    }
+
+
+}
+
+const totalPrice = pandaCost(1, -3, 3);
+console.log(totalPrice);
