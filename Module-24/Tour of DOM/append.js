4bd6df59241d411b675ced3ab69ccf348b2254ea
imrul099 <imrulmolla99@gmail.com>
2024-06-16 08:24:25 +0600

    added the javascript document

---
+++ Module-24/Tour of DOM/append.js
@@ -0,0 +1,27 @@
+//where to add...
+ const mainContainer = document.getElementById('main-container');
+ 
+//what to be added...
+
+ const section = document.createElement('section');
+ const h1 = document.createElement('h1');
+ h1.innerText = 'my favorite food list';
+ section.appendChild(h1);
+
+ const ul = document.createElement('ul');
+ const li1 = document.createElement('li');
+ li1.innerText = 'biriyani';
+ ul.appendChild(li1);
+
+ const li2 = document.createElement('li');
+ li2.innerText = 'tanduri';
+ ul.appendChild(li2);
+
+ const li3 = document.createElement('li');
+ li3.innerText = 'vaja mahc';
+ ul.appendChild(li3);
+
+ section.appendChild(ul);
+
+
+ mainContainer.appendChild(section);
---
+++ Module-24/Tour of DOM/dom.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border = '3px solid tomato';
+    section.style.backgroundColor = 'steelblue';
+    section.style.padding = '5px';
+    section.style.borderRadius = '8px';
+    section.style.marginBottom = '8px';
+};
+
+//const places = document.getElementById('place-interest');
+//places.style.backgroundColor = 'lightgray';
