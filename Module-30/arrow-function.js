963c3408cfc7faca6ad397b71d3298d31ba08be8
imrul099 <imrulmolla99@gmail.com>
2024-07-03 19:19:01 +0600

    added arrow-function

--- Module-30/arrow-function.js
+++ Module-30/arrow-function.js
@@ -21,4 +21,34 @@ const add4 = (num1, num2, num3) => num1 * num2 * num3;
 
 const sum = add3(5, 5);
 const sum2 = add4(2, 3, 4);
-console.log(sum,sum2)
+//console.log(sum,sum2);
+
+
+const difference = (x, y) => x - y;
+const multiply = (first, second, third) => first * second * third;
+//console.log(multiply(3,3,3));
+
+//Single or one parameter: I can use braket or no bracet;
+const getAge = (person) => person.name;
+const imrul = {name: 'molla', age: 23};
+const needAge = getAge(imrul);
+//console.log(needAge);
+
+
+const getThird = numbers => numbers[2];
+const third = getThird([77,33,72, 74]);
+console.log(third);
+
+
+//No parameter:
+const getPI = () => Math.PI;
+console.log(getPI())
+
+
+//large arrow function: if you want to get anything returned from this function. then you hav eto use the return keyword.
+const doMath = (x,y,z) => {
+    const sum = x + y + z;
+    const mult = x * y * z;
+    const result = sum + mult;
+    return result;
+}
