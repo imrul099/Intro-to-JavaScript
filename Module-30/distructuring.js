cb4dafb98927b88268c4b9b322e8234b4bad6b05
imrul099 <imrulmolla99@gmail.com>
2024-07-05 15:00:20 +0600

    added destructuring using js

---
+++ Module-30/distructuring.js
@@ -0,0 +1,39 @@
+const actor = {
+    name : 'ananto',
+    age : 35,
+    phone : 1761990398,
+    money : 525254
+}
+
+// const name = actor.name;
+// const age = actor.age;
+
+//if right side is an object left side of districtring will be object as well.
+
+//use propert name as a variable that contains the property value;
+const {phone, age : boyos} = actor;
+
+
+console.log(phone);
+console.log(phone);
+console.log(phone);
+console.log(bo)
+console.log(age)
+console.log(age)
+console.log(age)
+
+// array destrusturing.
+
+const numbers  = [33,54];
+const [first, second] = numbers;
+const [x, y] = [12,23];
+
+
+//function:advance;
+
+function doubleThem(a, b){
+    return [a*3, b*5];
+}
+
+const [prothom, ditiyo] = doubleThem(9,9);
+console.log(prothom, ditiyo)
