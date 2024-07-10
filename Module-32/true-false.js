df4757e9e042888633bf872ea1828d3d45e42246
imrul099 <imrulmolla99@gmail.com>
2024-07-10 22:27:31 +0600

    added true-false for javascript

---
+++ Module-32/true-false.js
@@ -0,0 +1,16 @@
+/**
+ * True:
+ * 1. true.
+ * 2. any number(+ve, -ve) will be true other than 0.
+ * 3. any string other than empty string.
+ * 4. '0', 'false'
+ * 5. {}
+ * 6. []
+ * 
+ * 
+ * FALSE:
+ * 1. false.
+ * 2. 0.
+ * 3. '' (empty string);
+ * 4. null
+ */
--- Module-32/undefined.js
+++ Module-32/undefined.js
@@ -6,6 +6,6 @@
  * 4. if return has nothing on the right side will return undefined.
  * 5. property that doesn't exists on an objects will give you undefined.
  * 6. accessing array elements outside of the index range will give you undefined.
- * deleting an element inside an array.[shouln't use delete, use slice];
- * set a value directly to undefined.[shouln't use]
+ *7. deleting an element inside an array.[shouln't use delete, use slice];
+ *8. set a value directly to undefined.[shouln't use]
  */
