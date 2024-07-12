71229f97bd3782f13beb76922f3a7f27b2ca0327
imrul099 <imrulmolla99@gmail.com>
2024-07-12 11:22:33 +0600

    added destructuring with array and object

--- Conceptual-Session-4/destructuring.js
+++ Conceptual-Session-4/destructuring.js
@@ -4,3 +4,18 @@ const [x,y,z] = number;
 
 
 console.log(x, y, z);
+
+
+
+//________________ Object Destrusturing ________________
+
+const person = {
+    name : 'hero alom',
+    age : 34,
+    phone : '029384793',
+    address : 'bari dharra',
+    p : [3,4,5,6]
+}
+
+const {phone, p: newArray, age} = person;
+console.log(newArray)
---
+++ Conceptual-Session-4/spreadObject.js
@@ -0,0 +1,13 @@
+const person = {
+    name : 'hero alom',
+    age : 34,
+    phone : '029384793',
+    address : 'bari dharra',
+    p : [3,4,5,6]
+}
+
+const person1 = {...person};
+person1.email = 'heroalom400@gmail.com'
+console.log(person1)
+
+//console.log(person)
